e8a63a220c0c77eff4053a7a67de0c7b3aa6ecc5
ZTL-UwU <zhangtianli2006@163.com>
2024-05-23 18:35:06 +0800

    feat: wip aside
    Signed-off-by: ZTL-UwU <zhangtianli2006@163.com>

--- components/layout/Aside.vue
+++ components/layout/Aside.vue
@@ -3,7 +3,11 @@
     class="fixed top-16 z-30 -ml-2 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 md:sticky md:block overflow-y-auto"
   >
     <UiScrollArea orientation="vertical" class="relative overflow-hidden h-full py-6 pr-6" type="auto">
-      Aside
+      <LayoutAsideTree :links="navigation" />
     </UiScrollArea>
   </aside>
 </template>
+
+<script setup lang="ts">
+const { navigation } = useContent();
+</script>
---
+++ components/layout/AsideTree.vue
@@ -0,0 +1,17 @@
+<template>
+  ↓
+  <ul>
+    <li v-for="link in links" :key="link._id">
+      {{ link.title }}
+      <LayoutAsideTree v-if="link.children" :links="link.children" />
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import type { NavItem } from '@nuxt/content/types';
+
+defineProps<{
+  links: NavItem[];
+}>();
+</script>
