d3dc7a2c16ff2ed90f5214b21c9db8c165cac3eb
ZTL-UwU <zhangtianli2006@163.com>
2024-06-02 00:38:14 +0800

    fix(mobile): code group tab overflow
    Signed-off-by: ZTL-UwU <zhangtianli2006@163.com>

--- components/content/CodeGroupHeader.vue
+++ components/content/CodeGroupHeader.vue
@@ -1,25 +1,30 @@
 <template>
-  <div class="border-b p-0.5 flex text-sm">
-    <UiTabs v-if="tabs" :default-value="tabs[0].label">
-      <UiTabsList>
-        <UiTabsTrigger
-          v-for="({ label, language }, i) in tabs"
-          :key="`${i}${label}`"
-          :value="label"
-          class="flex"
-          @click="updateTabs(i)"
-        >
-          <Icon v-if="getIcon(label, language)" :name="getIcon(label, language)!" class="self-center mr-1.5" />
-          {{ label }}
-        </UiTabsTrigger>
-      </UiTabsList>
-    </UiTabs>
-    <CodeCopy v-show="selected.label !== 'Preview'" class="self-center ml-auto mr-3" :code="selected.code" />
-  </div>
+  <UiScrollArea>
+    <div class="border-b p-0.5 flex text-sm relative overflow-x-auto">
+      <UiTabs v-if="tabs" :default-value="tabs[0].label">
+        <UiTabsList>
+          <UiTabsTrigger
+            v-for="({ label, language }, i) in tabs"
+            :key="`${i}${label}`"
+            :value="label"
+            class="flex"
+            @click="updateTabs(i)"
+          >
+            <Icon v-if="getIcon(label, language)" :name="getIcon(label, language)!" class="self-center mr-1.5" />
+            {{ label }}
+          </UiTabsTrigger>
+        </UiTabsList>
+      </UiTabs>
+      <CodeCopy v-show="selected.label !== 'Preview'" class="self-center ml-auto mr-3 pl-2" :code="selected.code" />
+    </div>
+    <ScrollBar orientation="horizontal" />
+  </UiScrollArea>
   <slot name="footer" />
 </template>
 
 <script setup lang="ts">
+import ScrollBar from '../ui/scroll-area/ScrollBar.vue';
+
 const props = defineProps<{
   tabs: ({
     label: string;
