abecfe040c10fb7765fbb5c6e1ec4b8dfc6439cb
ZTL-UwU <zhangtianli2006@163.com>
2024-06-02 11:33:36 +0800

    perf: remove tab from CodeGroupHeader
    Signed-off-by: ZTL-UwU <zhangtianli2006@163.com>

--- components/content/CodeGroupHeader.vue
+++ components/content/CodeGroupHeader.vue
@@ -1,20 +1,19 @@
 <template>
   <UiScrollArea>
     <div class="border-b p-0.5 flex text-sm relative overflow-x-auto">
-      <UiTabs v-if="tabs" :default-value="tabs[0].label">
-        <UiTabsList>
-          <UiTabsTrigger
-            v-for="({ label, language }, i) in tabs"
-            :key="`${i}${label}`"
-            :value="label"
-            class="flex"
-            @click="updateTabs(i)"
-          >
-            <Icon v-if="getIcon(label, language)" :name="getIcon(label, language)!" class="self-center mr-1.5" />
-            {{ label }}
-          </UiTabsTrigger>
-        </UiTabsList>
-      </UiTabs>
+      <div v-if="tabs" class="flex p-1">
+        <div
+          v-for="({ label, language }, i) in tabs"
+          :key="`${i}${label}`"
+          :value="label"
+          class="flex px-3 py-1.5 rounded-md text-muted-foreground transition-all duration-75 cursor-pointer"
+          :class="[activeTabIndex === i && 'bg-muted text-primary']"
+          @mousedown.left="updateTabs(i)"
+        >
+          <Icon v-if="getIcon(label, language)" :name="getIcon(label, language)!" class="self-center mr-1.5" />
+          {{ label }}
+        </div>
+      </div>
       <CodeCopy v-show="selected.label !== 'Preview'" class="self-center ml-auto mr-3 pl-2" :code="selected.code" />
     </div>
     <ScrollBar orientation="horizontal" />
