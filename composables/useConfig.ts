ce0deada058bd797f6830baae6cecc09aa9e125f
ZTL-UwU <zhangtianli2006@163.com>
2024-06-13 17:59:53 +0800

    feat: better config merger
    Signed-off-by: ZTL-UwU <zhangtianli2006@163.com>

--- composables/useConfig.ts
+++ composables/useConfig.ts
@@ -1,3 +1,153 @@
+import { defu } from 'defu';
+
+const defaultConfig: {
+  site: {
+    name: string;
+    description: string;
+    ogImage: string;
+  };
+  header: {
+    title: string;
+    showTitle: true;
+    logo: {
+      light: string;
+      dark: string;
+    };
+    darkModeToggle: true;
+    nav: ({
+      title: string;
+      to: string;
+      target: string;
+      links: ({
+        title: string;
+        to: string;
+        target: string;
+        description: string;
+      })[];
+    })[];
+    links: ({
+      icon: string;
+      to: string;
+      target: string;
+    })[];
+  };
+  aside: {
+    useLevel: boolean;
+    collapse: boolean;
+  };
+  main: {
+    breadCrumb: boolean;
+    showTitle: boolean;
+    codeIcon: {
+      [key: string]: string;
+    };
+  };
+  footer: {
+    credits: string;
+    links: ({
+      icon: string;
+      title: string;
+      to: string;
+      target: string;
+    })[];
+  };
+  toc: {
+    enable: boolean;
+    title: string;
+    links: ({
+      icon: string;
+      title: string;
+      to: string;
+      target: string;
+    })[];
+  };
+  search: {
+    enable: boolean;
+    inAside: boolean;
+  };
+} = {
+  site: {
+    name: 'shadcn-docs',
+    description: 'Beautifully designed Nuxt Content template built with shadcn-vue. Customizable. Compatible. Open Source.',
+    ogImage: '/hero.png',
+  },
+  header: {
+    title: 'shadcn-docs',
+    showTitle: true,
+    logo: {
+      light: '/logo.svg',
+      dark: '/logo-dark.svg',
+    },
+    darkModeToggle: true,
+    nav: [],
+    links: [],
+  },
+  aside: {
+    useLevel: true,
+    collapse: false,
+  },
+  main: {
+    breadCrumb: true,
+    showTitle: true,
+    codeIcon: {
+      'package.json': 'vscode-icons:file-type-node',
+      'tsconfig.json': 'vscode-icons:file-type-tsconfig',
+      '.npmrc': 'vscode-icons:file-type-npm',
+      '.editorconfig': 'vscode-icons:file-type-editorconfig',
+      '.eslintrc': 'vscode-icons:file-type-eslint',
+      '.eslintrc.cjs': 'vscode-icons:file-type-eslint',
+      '.eslintignore': 'vscode-icons:file-type-eslint',
+      'eslint.config.js': 'vscode-icons:file-type-eslint',
+      'eslint.config.mjs': 'vscode-icons:file-type-eslint',
+      'eslint.config.cjs': 'vscode-icons:file-type-eslint',
+      '.gitignore': 'vscode-icons:file-type-git',
+      'yarn.lock': 'vscode-icons:file-type-yarn',
+      '.env': 'vscode-icons:file-type-dotenv',
+      '.env.example': 'vscode-icons:file-type-dotenv',
+      '.vscode/settings.json': 'vscode-icons:file-type-vscode',
+      'nuxt': 'vscode-icons:file-type-nuxt',
+      '.nuxtrc': 'vscode-icons:file-type-nuxt',
+      '.nuxtignore': 'vscode-icons:file-type-nuxt',
+      'nuxt.config.js': 'vscode-icons:file-type-nuxt',
+      'nuxt.config.ts': 'vscode-icons:file-type-nuxt',
+      'nuxt.schema.ts': 'vscode-icons:file-type-nuxt',
+      'tailwind.config.js': 'vscode-icons:file-type-tailwind',
+      'tailwind.config.ts': 'vscode-icons:file-type-tailwind',
+      'vue': 'vscode-icons:file-type-vue',
+      'ts': 'vscode-icons:file-type-typescript',
+      'tsx': 'vscode-icons:file-type-typescript',
+      'mjs': 'vscode-icons:file-type-js',
+      'cjs': 'vscode-icons:file-type-js',
+      'js': 'vscode-icons:file-type-js',
+      'jsx': 'vscode-icons:file-type-js',
+      'md': 'vscode-icons:file-type-markdown',
+      'mdc': 'vscode-icons:file-type-markdown',
+      'py': 'vscode-icons:file-type-python',
+      'npm': 'vscode-icons:file-type-npm',
+      'pnpm': 'vscode-icons:file-type-pnpm',
+      'npx': 'vscode-icons:file-type-npm',
+      'yarn': 'vscode-icons:file-type-yarn',
+      'bun': 'vscode-icons:file-type-bun',
+      'yml': 'vscode-icons:file-type-yaml',
+      'json': 'vscode-icons:file-type-json',
+      'terminal': 'lucide:terminal',
+    },
+  },
+  footer: {
+    credits: '',
+    links: [],
+  },
+  toc: {
+    enable: true,
+    title: 'On This Page',
+    links: [],
+  },
+  search: {
+    enable: true,
+    inAside: false,
+  },
+};
+
 export function useConfig() {
   const appConfig = computed(() => useAppConfig()?.shadcnDocs || {});
 
@@ -7,14 +157,16 @@ export function useConfig() {
 
   return computed(
     () => {
-      const header = appConfig?.value?.header || {};
-      const main = appConfig?.value?.main || {};
-      const aside = appConfig?.value?.aside || {};
-      const footer = appConfig?.value?.footer || {};
-      const toc = appConfig?.value?.toc || {};
+      const processedConfig = defu(appConfig.value, defaultConfig);
+      const header = processedConfig.header;
+      const main = processedConfig.main;
+      const aside = processedConfig.aside;
+      const footer = processedConfig.footer;
+      const toc = processedConfig.toc;
 
       return {
         ...appConfig.value,
+        ...processedConfig,
         header: {
           ...header,
           ...navKeyFromPath(route.path, 'header', navigation.value || []),
--- content/index.md
+++ content/index.md
@@ -6,7 +6,7 @@ navigation: false
 ::hero
 ---
 announcement:
-  title: 'Release v0.1.3'
+  title: 'Release v0.2.0'
   icon: 'noto:party-popper'
   to: /getting-started
 actions:
