10a6d310937b06adf028008bd18c151596e33f4b
narr07 <dinarpermadi07@gmail.com>
2024-06-15 02:04:40 +0700

    feat: Add Toaster component for displaying toast notifications after copy code

--- components/content/CodeCopy.vue
+++ components/content/CodeCopy.vue
@@ -5,7 +5,7 @@
         v-if="copied === false"
         name="lucide:copy"
         class="self-center cursor-pointer text-muted-foreground hover:text-primary"
-        @click="copyCode"
+        @click="handleClick"
       />
       <Icon
         v-else
@@ -18,17 +18,27 @@
 </template>
 
 <script setup lang="ts">
+import { Button } from '@/components/ui/button';
+import { useToast } from '@/components/ui/toast/use-toast';
+import { Toaster } from '@/components/ui/toast';
+
 const props = defineProps<{
   code: string;
 }>();
 
+const { toast } = useToast();
+
 const { copy } = useClipboard({ source: props.code });
 const copied = ref(false);
-function copyCode() {
-  copy(props.code).then(
-    () => { copied.value = true; },
-  );
+
+async function handleClick() {
+  await copy(props.code);
+  copied.value = true;
+  toast({
+    description: 'Copied to clipboard!',
+  });
 }
+
 const checkIconRef = ref<HTMLElement>();
 onClickOutside(checkIconRef, () => {
   copied.value = false;
---
+++ components/ui/toast/use-toast.ts
@@ -0,0 +1,167 @@
+import { computed, ref } from 'vue';
+import type { Component, VNode } from 'vue';
+import type { ToastProps } from '.';
+
+const TOAST_LIMIT = 5;
+const TOAST_REMOVE_DELAY = 3000;
+
+export type StringOrVNode =
+  | string
+  | VNode
+  | (() => VNode);
+
+type ToasterToast = ToastProps & {
+  id: string;
+  title?: string;
+  description?: StringOrVNode;
+  action?: Component;
+};
+
+const actionTypes = {
+  ADD_TOAST: 'ADD_TOAST',
+  UPDATE_TOAST: 'UPDATE_TOAST',
+  DISMISS_TOAST: 'DISMISS_TOAST',
+  REMOVE_TOAST: 'REMOVE_TOAST',
+} as const;
+
+let count = 0;
+
+function genId() {
+  count = (count + 1) % Number.MAX_VALUE;
+  return count.toString();
+}
+
+type ActionType = typeof actionTypes;
+
+type Action =
+  | {
+    type: ActionType['ADD_TOAST'];
+    toast: ToasterToast;
+  }
+  | {
+    type: ActionType['UPDATE_TOAST'];
+    toast: Partial<ToasterToast>;
+  }
+  | {
+    type: ActionType['DISMISS_TOAST'];
+    toastId?: ToasterToast['id'];
+  }
+  | {
+    type: ActionType['REMOVE_TOAST'];
+    toastId?: ToasterToast['id'];
+  };
+
+interface State {
+  toasts: ToasterToast[];
+}
+
+const toastTimeouts = new Map<string, ReturnType<typeof setTimeout>>();
+
+function addToRemoveQueue(toastId: string, duration: number) {
+  if (toastTimeouts.has(toastId))
+    return;
+
+  const timeout = setTimeout(() => {
+    toastTimeouts.delete(toastId);
+    dispatch({
+      type: actionTypes.REMOVE_TOAST,
+      toastId,
+    });
+  }, duration);
+
+  toastTimeouts.set(toastId, timeout);
+}
+
+const state = ref<State>({
+  toasts: [],
+});
+
+function dispatch(action: Action) {
+  switch (action.type) {
+    case actionTypes.ADD_TOAST:
+      state.value.toasts = [action.toast, ...state.value.toasts].slice(0, TOAST_LIMIT);
+      break;
+
+    case actionTypes.UPDATE_TOAST:
+      state.value.toasts = state.value.toasts.map(t =>
+        t.id === action.toast.id ? { ...t, ...action.toast } : t,
+      );
+      break;
+
+    case actionTypes.DISMISS_TOAST: {
+      const { toastId } = action;
+
+      if (toastId) {
+        const toast = state.value.toasts.find(t => t.id === toastId);
+        if (toast) {
+          addToRemoveQueue(toastId, toast.duration ?? TOAST_REMOVE_DELAY);
+        }
+      } else {
+        state.value.toasts.forEach((toast) => {
+          addToRemoveQueue(toast.id, toast.duration ?? TOAST_REMOVE_DELAY);
+        });
+      }
+
+      state.value.toasts = state.value.toasts.map(t =>
+        t.id === toastId || toastId === undefined
+          ? {
+              ...t,
+              open: false,
+            }
+          : t,
+      );
+      break;
+    }
+
+    case actionTypes.REMOVE_TOAST:
+      state.value.toasts = state.value.toasts.filter(t => t.id !== action.toastId);
+      break;
+  }
+}
+
+function useToast() {
+  return {
+    toasts: computed(() => state.value.toasts),
+    toast,
+    dismiss: (toastId?: string) => dispatch({ type: actionTypes.DISMISS_TOAST, toastId }),
+  };
+}
+
+type Toast = Omit<ToasterToast, 'id'> & { duration?: number };
+
+function toast(props: Toast) {
+  const id = genId();
+
+  const update = (props: Partial<ToasterToast>) =>
+    dispatch({
+      type: actionTypes.UPDATE_TOAST,
+      toast: { ...props, id },
+    });
+
+  const dismiss = () => dispatch({ type: actionTypes.DISMISS_TOAST, toastId: id });
+
+  dispatch({
+    type: actionTypes.ADD_TOAST,
+    toast: {
+      ...props,
+      id,
+      open: true,
+      onOpenChange: (open: boolean) => {
+        if (!open)
+          dismiss();
+      },
+    },
+  });
+
+  setTimeout(() => {
+    dismiss();
+  }, props.duration ?? TOAST_REMOVE_DELAY);
+
+  return {
+    id,
+    dismiss,
+    update,
+  };
+}
+
+export { state, useToast, toast };
