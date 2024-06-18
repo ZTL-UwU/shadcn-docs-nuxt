d1bfdd4d9671440b2ccac2e4b660ff251774cbdc
ZTL-UwU <zhangtianli2006@163.com>
2024-06-18 18:31:57 +0800

    chore: fix type
    Signed-off-by: ZTL-UwU <zhangtianli2006@163.com>

--- app.config.ts
+++ app.config.ts
@@ -6,7 +6,7 @@ export default defineAppConfig({
       ogImage: '/hero.png',
     },
     theme: {
-      customizable: true,
+      customizable: false,
       color: 'zinc',
       radius: 0.5,
     },
@@ -18,48 +18,46 @@ export default defineAppConfig({
         dark: '/logo-dark.svg',
       },
       darkModeToggle: true,
-      nav: [
-        {
-          title: 'Docs',
-          links: [{
-            title: 'Getting Started',
-            to: '/getting-started',
-            description: 'Start building your document with shadcn-docs-nuxt',
-          }, {
-            title: 'API',
-            to: '/api',
-            description: 'Discover the configurations and exposed APIs.',
-          }],
-        },
-        {
-          title: 'Credits',
-          links: [{
-            title: 'shadcn-ui',
-            to: 'https://ui.shadcn.com/',
-            description: 'For the beautiful component design & docs design',
-            target: '_blank',
-          }, {
-            title: 'shadcn-vue',
-            to: 'https://www.shadcn-vue.com/',
-            description: 'For the vue port of shadcn-ui & some docs component source',
-            target: '_blank',
-          }, {
-            title: 'Docus',
-            to: 'https://docus.dev/',
-            description: 'For inspiration & some docs component source',
-            target: '_blank',
-          }, {
-            title: 'Nuxt Content',
-            to: 'https://content.nuxt.com/',
-            description: 'Content made easy for Vue Developers',
-            target: '_blank',
-          }],
-        },
-        {
-          title: 'Use This Template',
-          to: '/getting-started/installation',
-        },
-      ],
+      nav: [{
+        title: 'Docs',
+        links: [{
+          title: 'Getting Started',
+          to: '/getting-started',
+          description: 'Start building your document with shadcn-docs-nuxt',
+        }, {
+          title: 'API',
+          to: '/api',
+          description: 'Discover the configurations and exposed APIs.',
+          target: '_self',
+        }],
+      }, {
+        title: 'Credits',
+        links: [{
+          title: 'shadcn-ui',
+          to: 'https://ui.shadcn.com/',
+          description: 'For the beautiful component design & docs design',
+          target: '_blank',
+        }, {
+          title: 'shadcn-vue',
+          to: 'https://www.shadcn-vue.com/',
+          description: 'For the vue port of shadcn-ui & some docs component source',
+          target: '_blank',
+        }, {
+          title: 'Docus',
+          to: 'https://docus.dev/',
+          description: 'For inspiration & some docs component source',
+          target: '_blank',
+        }, {
+          title: 'Nuxt Content',
+          to: 'https://content.nuxt.com/',
+          description: 'Content made easy for Vue Developers',
+          target: '_blank',
+        }],
+      }, {
+        title: 'Use This Template',
+        to: '/getting-started/installation',
+        target: '_self',
+      }],
       links: [{
         icon: 'lucide:github',
         to: 'https://github.com/ZTL-UwU/shadcn-docs-nuxt',
