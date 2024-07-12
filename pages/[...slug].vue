19e76c1f1fe6a07f7207595b661b7376900b8277
ZTL-UwU <zhangtianli2006@163.com>
2024-07-12 16:45:28 +0800

    fix: aside position for md sized screens

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -5,7 +5,7 @@
       class="px-4 md:px-8 flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10"
       :class="[config.main.padded && 'container']"
     >
-      <aside class="fixed top-[102px] lg:top-[60px] z-30 -ml-2 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 md:sticky md:block overflow-y-auto">
+      <aside class="fixed top-[102px] md:top-[60px] z-30 -ml-2 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 md:sticky md:block overflow-y-auto">
         <LayoutAside :is-mobile="false" />
       </aside>
       <main class="relative py-6" :class="[config.toc.enable && 'lg:gap-10 lg:py-8 lg:grid lg:grid-cols-[1fr_200px]']">
