197b60b6cbf0fd9d78bf93afb8beb8f39c42214f
ZTL-UwU <zhangtianli2006@163.com>
2024-08-03 15:13:33 +0800

    fix: use function slots for codegroup tabs
    Signed-off-by: ZTL-UwU <zhangtianli2006@163.com>

--- components/content/CodeGroup.vue
+++ components/content/CodeGroup.vue
@@ -8,6 +8,6 @@ import Tabs from './Tabs.vue';
 const _slots = useSlots();
 function render() {
   const slots = _slots?.default?.() || [];
-  return h(Tabs, { variant: 'card' }, slots);
+  return h(Tabs, { variant: 'card' }, () => slots);
 }
 </script>
