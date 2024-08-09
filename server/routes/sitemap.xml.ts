dbe8cf1f460067275b4230709c382bf189239652
Miguel <miguel@glodival.vn>
2024-08-09 08:55:20 +0700

    update sitemap hostname

--- server/routes/sitemap.xml.ts
+++ server/routes/sitemap.xml.ts
@@ -5,7 +5,7 @@ export default defineEventHandler(async (event) => {
     // Fetch all documents
     const docs = await serverQueryContent(event).find()
     const sitemap = new SitemapStream({
-        hostname: 'https://example.com'
+        hostname: 'https://niemphat.top'
     })
 
     for (const doc of docs) {
