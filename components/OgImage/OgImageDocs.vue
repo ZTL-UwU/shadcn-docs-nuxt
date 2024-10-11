3cb6f2a7992a0e7432550376f5adf370e0f8b374
ZTL-UwU <zhangtianli2006@163.com>
2024-10-11 17:16:02 +0800

    feat(seo): add grid background to ogImage
    Signed-off-by: ZTL-UwU <zhangtianli2006@163.com>

--- components/OgImage/OgImageDocs.vue
+++ components/OgImage/OgImageDocs.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="flex size-full flex-col justify-center bg-white p-16">
+  <div
+    class="flex size-full flex-col justify-center bg-white p-16"
+    style="background-size: 120px 120px; background-image: linear-gradient(to right, #e5e7eb 1px, transparent 1px), linear-gradient(to bottom, #e5e7eb 1px, transparent 1px);"
+  >
     <div class="mb-4 flex">
       <img :src="config.header.logo.light" height="65">
       <span class="ml-4 self-center text-4xl font-bold">
