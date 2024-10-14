7d9b4ef65dc90ca24fc81d5116651f341d0e4590
ZTL-UwU <zhangtianli2006@163.com>
2024-10-14 18:11:40 +0800

    feat(components): shortcut
    Signed-off-by: ZTL-UwU <zhangtianli2006@163.com>

---
+++ components/content/Shortcut.vue
@@ -0,0 +1,19 @@
+<template>
+  <Kbd :size>
+    {{ computedValue }}
+  </Kbd>
+</template>
+
+<script setup lang="ts">
+const { value, size = 'sm' } = defineProps<{
+  value: string;
+  size?: 'xs' | 'sm' | 'md';
+}>();
+
+const metaSymbol = ref('Ctrl');
+onMounted(() => {
+  metaSymbol.value = navigator && navigator.userAgent && navigator.userAgent.match(/Macintosh;/) ? '⌘' : 'Ctrl';
+});
+
+const computedValue = computed(() => value === 'meta' ? metaSymbol : value);
+</script>
