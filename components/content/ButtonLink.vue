b60fd35c7923ac303d7f0b034f74afd17e0f0fb6
ZTL-UwU <zhangtianli2006@163.com>
2024-10-20 14:13:18 +0800

    feat(components): button link
    Signed-off-by: ZTL-UwU <zhangtianli2006@163.com>

---
+++ components/content/ButtonLink.vue
@@ -0,0 +1,24 @@
+<template>
+  <NuxtLink :to="to || href" :target="(blank && '_blank') || target">
+    <UiButton :variant :size>
+      <SmartIcon v-if="leftIcon" :name="leftIcon" class="mr-1" />
+      <ContentSlot unwrap="p" />
+      <SmartIcon v-if="rightIcon" :name="rightIcon" class="ml-1" />
+    </UiButton>
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+import type { ButtonVariants } from '../ui/button';
+
+defineProps<{
+  variant?: ButtonVariants['variant'];
+  size?: ButtonVariants['size'];
+  leftIcon?: string;
+  rightIcon?: string;
+  to?: string;
+  href?: string;
+  target?: Target;
+  blank?: boolean;
+}>();
+</script>
