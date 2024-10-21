8ffe92ab75b389d6a2d44a30404ee560d323684f
ZTL-UwU <zhangtianli2006@163.com>
2024-10-21 17:53:09 +0800

    fix: remove collapsible in aside
    Signed-off-by: ZTL-UwU <zhangtianli2006@163.com>

--- components/layout/AsideTreeItem.vue
+++ components/layout/AsideTreeItem.vue
@@ -1,42 +1,43 @@
 <template>
   <li>
-    <UiCollapsible v-if="link.children" v-model:open="isOpen">
-      <UiCollapsibleTrigger class="w-full text-left">
-        <div class="flex h-8 items-center gap-2 rounded-md p-2 text-sm text-foreground/80 hover:bg-muted hover:text-primary">
-          <SmartIcon
-            v-if="link.sidebar?.leftArrow"
-            name="lucide:chevron-down"
-            class="transition-transform"
-            :class="[!isOpen && '-rotate-90']"
-          />
+    <div v-if="link.children">
+      <button
+        class="flex h-8 w-full cursor-pointer items-center gap-2 rounded-md p-2 text-left text-sm font-medium text-foreground/80 hover:bg-muted hover:text-primary"
+        @click="isOpen = !isOpen"
+      >
+        <SmartIcon
+          v-if="link.sidebar?.leftArrow"
+          name="lucide:chevron-down"
+          class="transition-transform"
+          :class="[!isOpen && '-rotate-90']"
+        />
 
-          <SmartIcon
-            v-if="link.icon"
-            :name="link.icon"
-            :size="15"
-          />
+        <SmartIcon
+          v-if="link.icon"
+          :name="link.icon"
+          :size="15"
+        />
 
-          <span class="truncate text-nowrap">
-            {{ link.title }}
-          </span>
-          <span v-for="(badge, i) in link.navBadges" :key="i">
-            <Badge :variant="badge.variant" :type="badge.type" :size="badge.size ?? 'sm'">
-              {{ badge.value }}
-            </Badge>
-          </span>
+        <span class="truncate text-nowrap">
+          {{ link.title }}
+        </span>
+        <span v-for="(badge, i) in link.navBadges" :key="i">
+          <Badge :variant="badge.variant" :type="badge.type" :size="badge.size ?? 'sm'">
+            {{ badge.value }}
+          </Badge>
+        </span>
 
-          <SmartIcon
-            v-if="!link.sidebar?.leftArrow"
-            name="lucide:chevron-down"
-            class="ml-auto transition-transform"
-            :class="[!isOpen && '-rotate-90']"
-          />
-        </div>
-      </UiCollapsibleTrigger>
-      <UiCollapsibleContent>
+        <SmartIcon
+          v-if="!link.sidebar?.leftArrow"
+          name="lucide:chevron-down"
+          class="ml-auto transition-transform"
+          :class="[!isOpen && '-rotate-90']"
+        />
+      </button>
+      <div v-show="isOpen">
         <LayoutAsideTree :links="link.children" :level="level + 1" />
-      </UiCollapsibleContent>
-    </UiCollapsible>
+      </div>
+    </div>
     <NuxtLink
       v-else
       :to="link._path"
