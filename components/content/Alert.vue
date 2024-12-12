2cc8250632d7e8d7d7a83ec79872dc751884d398
Asterisk <spacedude609@pm.me>
2024-12-11 23:44:55 -0500

    feat: Add new alert types (#72)
    * add new alert types
    
    * Update alert.md
    
    * Update alert.md

--- components/content/Alert.vue
+++ components/content/Alert.vue
@@ -44,6 +44,8 @@ const {
 const typeTwClass = {
   default: '',
   info: 'border-sky-600 text-sky-600 [&>svg]:text-sky-600',
+  note: 'border-blue-700 text-blue-700 [&>svg]:text-blue-700',
+  example: 'border-violet-600 text-violet-600 [&>svg]:text-violet-600',
   warning: 'border-amber-600 text-amber-600 [&>svg]:text-amber-600',
   success: 'border-green-600 text-green-600 [&>svg]:text-green-600',
   danger: 'border-red-600 text-red-600 [&>svg]:text-red-600',
