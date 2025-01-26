243bab6239e0e4a015add333da1954437ec1a624
ZTL-UwU <zhangtianli2006@163.com>
2025-01-26 15:49:38 +0800

    perf: prerender index
    Signed-off-by: ZTL-UwU <zhangtianli2006@163.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,6 +19,9 @@ export default defineNuxtConfig({
     prefix: 'Ui',
     componentDir: join(currentDir, './components/ui'),
   },
+  routeRules: {
+    '/': { prerender: true },
+  },
   components: {
     dirs: [
       {
