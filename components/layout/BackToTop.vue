feb909e0c17dc2fa4fac60b4617e9784a9dadf9f
ZTL-UwU <zhangtianli2006@163.com>
2025-02-06 17:10:50 +0800

    refactor: smooth useWindowScroll
    Signed-off-by: ZTL-UwU <zhangtianli2006@163.com>

--- components/layout/BackToTop.vue
+++ components/layout/BackToTop.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="isBackToTopEnabled" class="w-fit">
-    <UiButton variant="link" class="text-sm font-semibold text-primary" @click="scrollToTop">
+    <UiButton variant="link" class="text-sm font-semibold text-primary" @click="y = 0">
       <div class="flex items-center gap-2 ">
         <SmartIcon name="lucide:arrow-up" />
         <span>Back to Top</span>
@@ -15,14 +15,5 @@ import { useWindowScroll } from '@vueuse/core';
 const config = useConfig().value.main;
 const isBackToTopEnabled = config.backToTop;
 
-const { y } = useWindowScroll();
-
-// Function to scroll to the top smoothly
-function scrollToTop() {
-  const scrollInterval = setInterval(() => {
-    y.value -= 50; // Scroll up in small steps
-    if (y.value <= 0)
-      clearInterval(scrollInterval); // Stop when at the top
-  }, 10); // Adjust speed by changing the interval time
-}
+const { y } = useWindowScroll({ behavior: 'smooth' });
 </script>
