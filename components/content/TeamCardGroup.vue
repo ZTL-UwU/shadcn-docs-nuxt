e506bce5c2358dc149640c9a07cc61f58cdb11ad
ZTL-UwU <zhangtianli2006@163.com>
2025-02-13 17:26:07 +0800

    fix: team card group margin top
    Signed-off-by: ZTL-UwU <zhangtianli2006@163.com>

--- components/content/TeamCardGroup.vue
+++ components/content/TeamCardGroup.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid grid-cols-[repeat(auto-fit,_minmax(270px,_1fr))] gap-6">
+  <div class="grid grid-cols-[repeat(auto-fit,_minmax(270px,_1fr))] gap-6 [&:not(:first-child)]:mt-5">
     <slot />
   </div>
 </template>
