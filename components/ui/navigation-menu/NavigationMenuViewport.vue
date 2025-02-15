730c5a6a28ad3590d56fd3d744693ffe727836be
ZTL-UwU <zhangtianli2006@163.com>
2025-02-15 21:36:49 +0800

    fix: navigation menu & search dialog animation
    Signed-off-by: ZTL-UwU <zhangtianli2006@163.com>

--- components/ui/navigation-menu/NavigationMenuViewport.vue
+++ components/ui/navigation-menu/NavigationMenuViewport.vue
@@ -4,7 +4,7 @@
       v-bind="forwardedProps"
       :class="
         cn(
-          'origin-top-center relative mt-1.5 h-[--radix-navigation-menu-viewport-height] w-full overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-lg data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-90 md:w-[--radix-navigation-menu-viewport-width]',
+          'origin-top-center relative mt-1.5 h-[var(--radix-navigation-menu-viewport-height)] w-full overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-lg data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-90 md:w-[var(--radix-navigation-menu-viewport-width)]',
           props.class,
         )
       "
