046afd9e0460e93da351ad25bc192c51a9c03de7
ZTL-UwU <zhangtianli2006@163.com>
2025-03-09 16:24:29 +0800

    chore: upgrade to nuxt 3.16.0
    Signed-off-by: ZTL-UwU <zhangtianli2006@163.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -35,6 +35,11 @@ export default defineNuxtConfig({
   css: [
     join(currentDir, './assets/css/themes.css'),
   ],
+  tailwindcss: {
+    config: {
+      content: ['./pages/**/*.{vue,js,ts}', './components/**/*.{vue,js,ts}'],
+    },
+  },
   content: {
     documentDriven: true,
     highlight: {
@@ -77,9 +82,6 @@ export default defineNuxtConfig({
         baseUrl: '.',
       },
     },
-  },
-  build: {
-    transpile: ['shiki', 'ohash'],
   },
   compatibilityDate: '2024-07-05',
 });
