27ada8e8d885657b082115cf3fcc75edf5cb5cb7
Benoit Rospars <benoit.rospars@inria.fr>
2025-03-18 14:23:38 +0100

    Add support for localized prev/next

--- components/layout/PrevNext.vue
+++ components/layout/PrevNext.vue
@@ -1,12 +1,37 @@
 <template>
   <div v-if="page.prevNext ?? true" class="border-t pt-6 lg:flex lg:flex-row">
-    <LayoutPrevNextButton :prev-next="prev" side="left" />
+    <LayoutPrevNextButton :prev-next="localizedPrev" side="left" />
     <span class="flex-1" />
-    <LayoutPrevNextButton :prev-next="next" side="right" />
+    <LayoutPrevNextButton :prev-next="localizedNext" side="right" />
   </div>
 </template>
 
 <script setup lang="ts">
-const { page } = useContent();
-const { prev, next } = useContent();
+let { page, next, prev } = useContent();
+const { locale, defaultLocale, availableLocales } = useI18n();
+
+const otherLocales = availableLocales.filter(l => l !== defaultLocale);
+
+const localizedPrev = computed(() => {
+  if (locale.value === defaultLocale) {
+    if (otherLocales.some(l => prev.value?._path.startsWith(`/${l}/`))) return null
+  } else {
+    if (!prev.value?._path.startsWith(`/${locale.value}`)) {
+      return null;
+    }
+  }
+  return prev.value;
+})
+const localizedNext = computed(() => {
+  if (locale.value === defaultLocale) {
+    if (otherLocales.some(l => next.value?._path.startsWith(`/${l}/`))) return null
+  } else {
+    if (!next.value?._path.startsWith(`/${locale.value}`)) {
+      return null;
+    }
+  }
+  return next.value;
+})
+
+
 </script>
