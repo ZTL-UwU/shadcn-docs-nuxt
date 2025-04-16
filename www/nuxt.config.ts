bcee73fec96c21facdd3c2d7e741212ce418d7a0
ZTL-UwU <zhangtianli2006@163.com>
2025-04-16 11:02:44 +0800

    feat(www): enable component meta completion
    Signed-off-by: ZTL-UwU <zhangtianli2006@163.com>

--- www/nuxt.config.ts
+++ www/nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: [
+    'nuxt-component-meta',
+  ],
   extends: ['..'],
   i18n: {
     defaultLocale: 'en',
