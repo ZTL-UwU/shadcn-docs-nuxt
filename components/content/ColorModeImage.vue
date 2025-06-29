9801b051d545497116dd4767b47ade4507aa9651
ZTL-UwU <zhangtianli2006@gmail.com>
2025-06-29 16:11:37 +0800

    feat: color mode image

---
+++ components/content/ColorModeImage.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="[&:not(:first-child)]:mt-6">
+    <div class="dark:hidden">
+      <ProseImg
+        v-bind="{
+          src: props.srcLight,
+          alt: props.alt,
+          width: props.width,
+          height: props.height,
+          lifted: props.lifted,
+          noZoom: props.noZoom,
+        }"
+      />
+    </div>
+    <div class="hidden dark:block">
+      <ProseImg
+        v-bind="{
+          src: props.srcDark,
+          alt: props.alt,
+          width: props.width,
+          height: props.height,
+          lifted: props.lifted,
+          noZoom: props.noZoom,
+        }"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  srcLight: {
+    type: String,
+    default: '',
+  },
+  srcDark: {
+    type: String,
+    default: '',
+  },
+  alt: {
+    type: String,
+    default: '',
+  },
+  width: {
+    type: [String, Number],
+    default: undefined,
+  },
+  height: {
+    type: [String, Number],
+    default: undefined,
+  },
+  lifted: {
+    type: Boolean,
+    default: false,
+  },
+  noZoom: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
