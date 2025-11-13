0e7854ad7d6ccd9ec1512149ea3da584e9e59f02
ZTL-UwU <zhangtianli2006@gmail.com>
2025-11-13 01:42:06 -0800

    chore: release v1.1.6
    Signed-off-by: ZTL-UwU <zhangtianli2006@gmail.com>

--- taze.config.ts
+++ taze.config.ts
@@ -4,13 +4,7 @@ export default defineConfig({
   force: true,
   write: true,
   install: true,
-  mode: 'major',
   recursive: true,
-  exclude: [
-    'nuxt',
-    '@nuxt/kit',
-    '@nuxt/icon',
-  ],
   // ignore paths for looking for package.json in monorepo
   ignorePaths: [
     '**/node_modules/**',
