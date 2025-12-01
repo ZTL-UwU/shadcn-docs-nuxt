25294f9cfd9f5b9535085692f7c3f51f24887d51
Bin <fuwb0407@gamil.com>
2025-12-01 15:50:17 +0800

    fix(NavigationMenuViewport): eslint fix

--- components/ui/navigation-menu/NavigationMenuViewport.vue
+++ components/ui/navigation-menu/NavigationMenuViewport.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="absolute left-0 top-full flex justify-center">
     <NavigationMenuViewport
-        v-bind="forwardedProps"
-        :class="
+      v-bind="forwardedProps"
+      :class="
         cn(
           'left-[var(--reka-navigation-menu-viewport-left)]',
           'origin-top-center relative mt-1.5 h-[var(--reka-navigation-menu-viewport-height)] w-full overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-lg data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-90 md:w-[var(--reka-navigation-menu-viewport-width)] data-[state=closed]:fade-out data-[state=open]:fade-in transition-[width,height] duration-300',
